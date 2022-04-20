5f287ebeaf35b75986395ff39ec6605db84ddadc
gavinmorrow <gavinfmorrow@gmail.com>
2022-04-20 11:05:56 -0400

    Init iOS App

---
+++ FavoriteBooks/FavoriteBooks/FavoriteBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBooksApp.swift
+//  FavoriteBooks
+//
+//  Created by Gavin Morrow on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
